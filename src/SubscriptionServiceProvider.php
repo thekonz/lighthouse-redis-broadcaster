aafe7e5d49396e9cca16488845be4803bc6e6efc
Konstantin Duzcek <zinnenk@gmail.com>
2020-05-02 15:39:13 +0200

    add authorizer to support presence channels

--- src/SubscriptionServiceProvider.php
+++ src/SubscriptionServiceProvider.php
@@ -4,11 +4,13 @@ namespace thekonz\LighthouseRedisBroadcaster;
 
 use Illuminate\Support\ServiceProvider;
 use Nuwave\Lighthouse\Subscriptions\BroadcastManager as BaseBroadcastManager;
+use Nuwave\Lighthouse\Subscriptions\Contracts\AuthorizesSubscriptions;
 use Nuwave\Lighthouse\Subscriptions\Contracts\StoresSubscriptions;
 use thekonz\LighthouseRedisBroadcaster\Broadcasting\BroadcastManager;
 use thekonz\LighthouseRedisBroadcaster\Broadcasting\RedisBroadcaster;
 use thekonz\LighthouseRedisBroadcaster\Console\LighthouseSubscribeCommand;
 use thekonz\LighthouseRedisBroadcaster\Contracts\Broadcaster;
+use thekonz\LighthouseRedisBroadcaster\Routing\Authorizer;
 use thekonz\LighthouseRedisBroadcaster\Storage\Manager;
 
 class SubscriptionServiceProvider extends ServiceProvider
@@ -30,6 +32,7 @@ class SubscriptionServiceProvider extends ServiceProvider
 
         $this->mergeConfigFrom(__DIR__ . '/config.php', 'lighthouse.subscriptions.broadcasters.redis');
 
+        $this->app->singleton(AuthorizesSubscriptions::class, Authorizer::class);
         $this->app->singleton(Broadcaster::class, RedisBroadcaster::class);
         $this->app->singleton(BaseBroadcastManager::class, BroadcastManager::class);
         $this->app->singleton(StoresSubscriptions::class, Manager::class);
